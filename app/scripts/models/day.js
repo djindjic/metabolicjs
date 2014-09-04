6dd02e3b8a1e476f1ae3a3ca06b1646331eff8ff
Aleksandar Djindjic <djindjic@gmail.com>
2014-09-04 09:50:29 +0200

    fix grunt warnings

--- app/scripts/models/day.js
+++ app/scripts/models/day.js
@@ -3,19 +3,19 @@
 angular.module('nutnexApp.models')
   .factory('Day', ['InterfaceUtil', 'TrainingInterface', function (InterfaceUtil, TrainingInterface) {
       function Day() {
-          var _meals = [],
-              _trainings = [];
+        var _meals = [],
+            _trainings = [];
 
-          Object.defineProperty(this, 'meals', {
-        get: function() { return _meals; },
-        set: function(val) { _meals = val; }
-      });
+        Object.defineProperty(this, 'meals', {
+          get: function() { return _meals; },
+          set: function(val) { _meals = val; }
+        });
 
         Object.defineProperty(this, 'trainings', {
-        get: function() { return _trainings; },
-        set: function(val) { _trainings = val; }
-      });
-      };
+          get: function() { return _trainings; },
+          set: function(val) { _trainings = val; }
+        });
+      }
     
       Day.prototype = {
         addTraining: function(training) {
