ba9f89b85ab48886faeecee0338b995237a30ae4
Aleksandar Djindjic <djindjic@gmail.com>
2014-10-24 17:24:10 +0200

    reasearch differences between traceur and 6to5

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
 var gulp    = require('gulp'),
-	Promise = require('promise'),	
-	del     = require('del'),
-	$       = require('gulp-load-plugins')();
-	to5     = require('gulp-6to5');
+    Promise = require('promise'),   
+    del     = require('del'),
+    $       = require('gulp-load-plugins')();
+    to5     = require('gulp-6to5');
 
 var clean = function (paths) {
   return new Promise(function (fulfil) {
@@ -15,25 +15,25 @@ var clean = function (paths) {
 };
 
 var sixto5 = function () {
-	return new Promise(function (fulfil) {
-		$.util.log('-to5');
-	    gulp.src('src/**/*.js')
-	        .pipe(to5({ modules: "amd" }))
-	        //.pipe($.concat('all.js'))
-	        .pipe(gulp.dest('dist-to5'));
-	});
+  return new Promise(function (fulfil) {
+    $.util.log('-to5');
+    gulp.src('src/**/*.js')
+      .pipe(to5({ modules: "amd" }))
+      //.pipe($.concat('all.js'))
+      .pipe(gulp.dest('dist-to5'));
+  });
 };
 
 // var traceur = function () {
-// 	return new Promise(function (fulfil) {
-// 		$.util.log('-traceur');
-// 	    gulp.src('src/**/*.js')
-// 	        .pipe($.sourcemaps.init())
-// 	        .pipe($.traceur())
-// 	        .pipe($.concat('all.js'))
-// 	        .pipe($.sourcemaps.write())
-// 	        .pipe(gulp.dest('dist-traceur'));
-// 	});
+//  return new Promise(function (fulfil) {
+//      $.util.log('-traceur');
+//      gulp.src('src/**/*.js')
+//          .pipe($.sourcemaps.init())
+//          .pipe($.traceur())
+//          .pipe($.concat('all.js'))
+//          .pipe($.sourcemaps.write())
+//          .pipe(gulp.dest('dist-traceur'));
+//  });
 // };
 
 gulp.task('default',
