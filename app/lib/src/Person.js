5bdb6c3842bdaf9a8bba46df8adda1d5e0da6e32
Aleksandar Djindjic <djindjic@gmail.com>
2014-11-30 19:53:23 +0100

    person class

--- app/lib/src/Person.js
+++ app/lib/src/Person.js
@@ -1,5 +1,20 @@
 export class Person {
-  constructor() {
-  	this.age = 34;
+  constructor({yearOfBirth: yearOfBirth, gender: gender, height: height, weight: weight}) {
+  	this._yearOfBirth = yearOfBirth;
+  	this._gender = gender;
+  	this._height = height;
+  	this._weight = weight;
   }
+
+  set yearOfBirth(value) { this._yearOfBirth = value; }
+  get yearOfBirth() { return this._yearOfBirth; }
+
+  set gender(value) { this._gender = value; }
+  get gender() { return this._gender; }
+
+  set height(value) { this._height = value; }
+  get height() { return this._height; }
+
+  set weight(value) { this._weight = value; }
+  get weight() { return this._weight; }
 }
