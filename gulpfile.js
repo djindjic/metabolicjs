67fb0b5ef1d042233b57206ad96ea943830a4b2d
Aleksandar Djindjic <djindjic@gmail.com>
2014-12-24 11:00:58 +0100

    ver

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,16 @@
-var gulp  = require('gulp'),
-    shell = require('gulp-shell'),
-    watch = require('gulp-watch');
- 
+var gulp    = require('gulp'),
+    shell   = require('gulp-shell'),
+    watch   = require('gulp-watch'),
+    bump    = require('gulp-bump'),
+    version = require('./package.json').version;
+
+
+gulp.task('bump-version', function(){
+  return gulp.src(['./package.json'])
+    .pipe(bump({type:'patch'})) //major|minor|patch|prerelease
+    .pipe(gulp.dest('./'));
+});
+
 gulp.task('default', function(cb) {
-  watch(['lib/**/*'], shell.task(['jspm link github:djindjic/metabolicjs@0.0.2 -y']));
+  watch(['lib/**/*'], shell.task(['jspm link github:djindjic/metabolicjs@' + version + ' -y']));
 });
