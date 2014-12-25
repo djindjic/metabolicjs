7132736bbb9739db2a948016bfeb54bc595dc36f
Aleksandar Djindjic <djindjic@gmail.com>
2014-12-25 16:14:24 +0100

    decimal.js from github

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 System.config({
   "paths": {
     "*": "*.js",
-    "github:*": "jspm_packages/github/*.js"
+    "github:*": "jspm_packages/github/*.js",
+    "npm:*": "jspm_packages/npm/*.js"
   }
 });
 
@@ -12,6 +13,13 @@ System.config({
     "github:aurelia/dependency-injection@0.1.1": {
       "aurelia-metadata": "github:aurelia/metadata@0.1.1",
       "es6-shim": "github:paulmillr/es6-shim@0.21.1"
+    },
+    "github:jspm/nodelibs@0.0.7": {
+      "Base64": "npm:Base64@0.2.1",
+      "base64-js": "npm:base64-js@0.0.7",
+      "ieee754": "npm:ieee754@1.1.4",
+      "inherits": "npm:inherits@2.0.1",
+      "json": "github:systemjs/plugin-json@0.1.0"
     }
   }
 });
