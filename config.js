3c51406bcb91c4fb46b6262a3b28ede5df655bcc
Aleksandar Djindjic <djindjic@gmail.com>
2015-01-04 23:31:13 +0100

    es6++ options - annotations

--- config.js
+++ config.js
@@ -15,14 +15,8 @@ System.config({
     "github:aurelia/dependency-injection@0.1.1": {
       "aurelia-metadata": "github:aurelia/metadata@0.1.1",
       "es6-shim": "github:paulmillr/es6-shim@0.21.1"
-    },
-    "github:jspm/nodelibs@0.0.7": {
-      "Base64": "npm:Base64@0.2.1",
-      "base64-js": "npm:base64-js@0.0.7",
-      "ieee754": "npm:ieee754@1.1.4",
-      "inherits": "npm:inherits@2.0.1",
-      "json": "github:systemjs/plugin-json@0.1.0"
     }
   }
 });
 
+System.traceurOptions.annotations = true;
