fed8707c66d351f19d38225b1a88d697721b9c3e
Aleksandar Djindjic <djindjic@gmail.com>
2015-01-07 12:24:44 +0100

    apply static define from forwardable

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ System.config({
   "map": {
     "aurelia-dependency-injection": "github:aurelia/dependency-injection@0.1.1",
     "decimal.js": "github:MikeMcl/decimal.js@4.0.1",
-    "forwardablejs": "github:djindjic/forwardablejs@0.1.14",
+    "forwardablejs": "github:djindjic/forwardablejs@0.1.16",
     "github:aurelia/dependency-injection@0.1.1": {
       "aurelia-metadata": "github:aurelia/metadata@0.1.1",
       "es6-shim": "github:paulmillr/es6-shim@0.21.1"
